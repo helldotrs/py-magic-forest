c5e9e73a18af05b271fd66e5c2700d50fdbf2ef3
Rebekah <macrosystem@proton.me>
2024-02-12 01:48:51 +0100

    Update mvp.py

--- mvp.py
+++ mvp.py
@@ -1,10 +1,22 @@
 # d - dark
 # p - player
 
-# maps
+# map1
 # ddd dpd ddd
 # ddd ddd ddd
 # ddd ddd ddd
+
+#qwerty layout:
+# QWERT   YUIOP
+#  ASDFG   HJKL
+#   ZXCVB   NM
+
+# qwerty keymap:
+# movement: asdf
+# map: q
+# inventory: w
+# attack: e
+# magic: r
 
 class Player(self):
   self.pos_x = 1
